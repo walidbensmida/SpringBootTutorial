5a8954f4ef3ba0f43a36de8fc25dbcc7b0d205b1
wbensmida <walidbensmida@hotmail.fr>
2019-11-04 11:10:44 +0100

    Spring Boot first tutorial

---
+++ src/main/java/com/example/demo/dao/PersonDataAccessService.java
@@ -0,0 +1,39 @@
+package com.example.demo.dao;
+
+import com.example.demo.model.Person;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+public class PersonDataAccessService implements PersonDao {
+    @Override
+    public int insertPerson(UUID id, Person person) {
+        return 0;
+    }
+
+    @Override
+    public int insertPerson(Person person) {
+        return 0;
+    }
+
+    @Override
+    public List<Person> selectAllPeople() {
+        return null;
+    }
+
+    @Override
+    public Optional<Person> selectPersonById(UUID id) {
+        return Optional.empty();
+    }
+
+    @Override
+    public int deletePerson(UUID id) {
+        return 0;
+    }
+
+    @Override
+    public int updatePersonById(UUID id, Person person) {
+        return 0;
+    }
+}
